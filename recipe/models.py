b23d250a3834b3565cff97bc8d72f7954b058351
Nguyen Tuong Bach <87748138+Bach2011@users.noreply.github.com>
2025-12-01 09:39:23 +0000

    constructing models, urls and views function

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser, models.Model):
+    def __str__(self):
+        return f'{self.username}'
+class Ingridient(models.Model):
+    name = models.CharField()
+class Recipe(models.Model):
+    name = models.CharField()
+    picture = models.CharField(default = "")
+    ingridents = models.ManyToManyField(Ingridient, related_name="food")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import User, Ingridient, Recipe
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+def index(request):
+    return HttpResponse("Hi bro!")
+def recipes(request):
+    if(request.method == "POST"):
+        if(request.POST.get("search") == 1):
+            pass
+        else:
+            pass
+        pass
+    elif(request.method == "GET"):
+        pass
+def recipe(request):
+    pass
