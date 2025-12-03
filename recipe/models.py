522c4b58ff3b8ba42fdff79acff27298c086af77
Nguyen Tuong Bach <87748138+Bach2011@users.noreply.github.com>
2025-12-03 13:29:09 +0000

    update database fields

--- recipe/models.py
+++ recipe/models.py
@@ -10,4 +10,6 @@ class Recipe(models.Model):
     name = models.CharField()
     picture = models.CharField(default = "")
     ingridents = models.ManyToManyField(Ingridient, related_name="food")
+    description = models.CharField(default = "")
+    instruction = models.CharField(default = "")
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
